bae06cff1ca867a1e5239bb35fbac91da6a3f865
Nzombie Gaming <nzombiegaming@gmail.com>
2018-02-17 10:59:50 -0700

    Strings!!!

---
+++ string.cpp
@@ -0,0 +1,18 @@
+//This will teach you how to use a string
+
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main(){
+
+	string fName;
+
+	cout<<"Enter your first name\n";
+	cin>> fName;
+	cout<< "Your name is "<< fName <<" " << endl;
+
+
+	return 0;
+}
