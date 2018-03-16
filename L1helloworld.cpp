ea7055ea92b4ae81b1d945e242860363cc8392a6
Nzombie Gaming <nzombiegaming@gmail.com>
2018-03-16 11:52:40 -0600

    Organized files

--- L1helloworld.cpp
+++ L1helloworld.cpp
@@ -3,3 +3,14 @@
 //First you will notice that I am putting // before each line. 
 // "//" allow you to comment out code meaning it will not be read by the compiler
 //The compiler is a wonderful thing that you will learn to love and hate.
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	cout<<"Hello World"; //<< endl;	
+
+	return 0;
+}
