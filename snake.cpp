f6146788205dccc414a88d7d8d144aa60069bb11
Nzombie Gaming <nzombiegaming@gmail.com>
2018-04-24 12:30:56 -0600

    SNAKE SNAKE SNNNAAAKKKEEE!!! (Function examples)

---
+++ snake.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void howtofunction()
+{
+	cout<<"this is a function that can be called at anytime in your code" <<endl;
+}
+
+void menu()
+{
+	cout<<" WELCOME TO SNAKE\n";
+}
+
+
+int main()
+{
+	howtofunction();
+ 	menu();
+	return 0;
+}
